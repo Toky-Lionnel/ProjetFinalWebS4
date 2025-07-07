0f96492c22eaa6b81332cd7d56b28bf60489c456
Faniry_Ntsooa <faniry.rakotobe3@gmail.com>
2025-07-07 16:03:46 +0300

    script

--- sql/conception.sql
+++ sql/conception.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Etablissement_Financier (
+CREATE TABLE finance_Etablissement_Financier (
    id INT,
    nom_etablissement VARCHAR(50),
    fonds INT,
    PRIMARY KEY(id)
 );
 
-CREATE TABLE Type_Pret (
+CREATE TABLE finance_Type_Pret (
    id INT,
    libelle VARCHAR(50),
    taux DECIMAL(10,2),
@@ -13,14 +13,14 @@ CREATE TABLE Type_Pret (
    PRIMARY KEY(id)
 );
 
-CREATE TABLE Clients (
+CREATE TABLE finance_Clients (
    id INT,
    nom VARCHAR(50),
    email VARCHAR(50),
    PRIMARY KEY(id)
 );
 
-CREATE TABLE Pret (
+CREATE TABLE finance_Pret (
    id_type_pret INT,
    id_client INT,
    id INT,
@@ -33,19 +33,19 @@ CREATE TABLE Pret (
    FOREIGN KEY(id_client) REFERENCES Clients(id)
 );
 
-CREATE TABLE Type_Transaction (
+CREATE TABLE finance_Type_Transaction (
    id INT,
    libelle VARCHAR(50),
    PRIMARY KEY(id)
 );
 
-CREATE TABLE Type_Mouvement (
+CREATE TABLE finance_Type_Mouvement (
    id INT,
    libelle VARCHAR(50),
    PRIMARY KEY(id)
 );
 
-CREATE TABLE Transaction (
+CREATE TABLE finance_Transaction (
    id_type_pret INT,
    id_client INT,
    id_pret INT,
